b671a2f409cbd37de39e74dcdb12239f2cb6d6a3
Talha Iftikar <92679085+TalhaIftikharAli@users.noreply.github.com>
2023-09-17 00:52:52 +0500

    Add files via upload

---
+++ Data Type Conversion Project.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# """
+# Created on Sun Sep 17 00:44:08 2023
+
+# @author: talha.i
+# """
+
+# Data Type Conversion Project
+
+two_digit_number = input("Enter a two digit number:\n")
+
+first_digit = int(two_digit_number[0])
+
+second_digit = int(two_digit_number[1])
+
+added_digits = str(first_digit + second_digit)
+
+print("The sum of the entered two digit numbers is " + added_digits)
